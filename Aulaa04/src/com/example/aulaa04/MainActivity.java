6e19a21ed1d3493ecbe97740221ab78154d56bce
silvad2 <silvad2@BRDK16051170Z0E.GLOBAL.ECOLAB.CORP>
2016-09-30 11:02:22 -0300

    Aula04 Calcularora Layout

--- Aulaa04/src/com/example/aulaa04/MainActivity.java
+++ Aulaa04/src/com/example/aulaa04/MainActivity.java
@@ -17,26 +17,6 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		final Calcular calc = new Calcular();
-		Button botaoSoma = (Button) findViewById(R.id.bntSoma);
-		Button botaoSubtrair = (Button) findViewById(R.id.bntSubtrai);
-		final Button botaoDividir = (Button) findViewById(R.id.bntDividir);
-		Button botaoMultiplicar = (Button) findViewById(R.id.bntMultiplica);
-		TextView va = (TextView) botaoDividir.getText();
-		final EditText result = (EditText) findViewById(R.id.TextViewResult);
-		botaoSoma.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				TextView operador = (TextView) findViewById(R.id.textViewOperador);
-				operador.setText((CharSequence) botaoDividir.getText());
-				EditText valor1 = (EditText) findViewById(R.id.editText1);
-				EditText valor2 = (EditText) findViewById(R.id.editText2);
-				
-				result.setText(calc.somar(valor1.toString(), valor2.toString()));
-				
-			}
-		});
 		
 	}
 
